7aae659e046de0c269c99a3f4be693d49c23c721
trungleadplus <94898503+trungleadplus@users.noreply.github.com>
2025-07-01 14:16:10 +0700

    Build 1

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,10 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors({
   origin: 'http://localhost:5173',
